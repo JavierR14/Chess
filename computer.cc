095b3a3a3655ebc5cde8e2444bca9e770c6f93c8
smcanham <smcanham@student.cs.uwaterloo.ca>
2015-12-03 16:30:15 -0500

    updates

--- computer.cc
+++ computer.cc
@@ -2,6 +2,7 @@
 #include "board.h"
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -10,11 +11,15 @@ Computer::Computer(int playerID, int level, Board *b): Player(0, playerID, 1, b,
 }
 
 bool Computer::movePiece(){
+	srand(time(NULL));
+	int skip = (rand() % 4);
 	if(level == 1){
+/*
 		for(int i=0; i<8; i++){
 			for(int z=0; z<8; z++){
-				if(b->theBoard[i][z]->getOwner() == this->playerID()){
-					if(b->theBoard[i][z]->pieceID() == 'P'){
+				if(b->getPiece(i,z) !=NULL){
+				if(b->getPiece(i,z)->getOwner() == this->playerID()){
+					if(b->getPiece(i,z)->pieceID() == 'P'){
 						if(b->getPiece(i,z)->makeMove(i,z,i-1,z-1)){
 							b->updateMove(i,z,i-1,z-1);
 							return true;
@@ -218,16 +223,79 @@ bool Computer::movePiece(){
 					}
 				}
 			}
+			}
 		}
+*/
+	for(int i=0; i<8; i++){
+		for(int j=0; j<8; j++){
+			if((b->getPiece(i,j) !=NULL) && 
+				(b->getPiece(i,j)->getOwner() == this->playerID())){
+				for(int x=0; x<8; x++){
+					for(int y=0; y<8; y++){
+						if((skip==0)
+							&& (b->getPiece(i,j)->makeMove(i,j,x,y))){
+							b->updateMove(i,j,x,y);
+							return true;
+						}
+						if(b->getPiece(i,j)->makeMove(i,j,x,y)){
+							skip--;
+						}
+					}
+				}
+			}
+		}
+	}  
 	}
+	else if(level ==2){
+	int count = 0;
+	while(count < 2){
+		for(int i=0; i<8; i++){
+			for(int j=0; j<8; j++){
+				if((b->getPiece(i,j) != NULL) && 
+					(b->getPiece(i,j)->getOwner() == this->playerID())){
+				for(int x=0; x<8; x++){
+					for(int y=0; y<8; y++){
+						if((b->getPiece(x,y) !=NULL) &&
+						(b->getPiece(x,y)->getOwner() != this->playerID())){
+							if(b->getPiece(i,j)->makeMove(i,j,x,y)){
+								b->updateMove(i,j,x,y);
+								count =2;
+								return true;
+							}
+						}
+						if((b->getPiece(i,j)->checkMove(i,j,x,y)) &&
+						(b->getPiece(i,j)->checkMove(x,y,this->kingRow, this->kingCol))){
+							b->getPiece(i,j)->makeMove(i,j,x,y);
+							b->updateMove(i,j,x,y);
+							return true;
+						}	
+						if(count==1){
+							if(b->getPiece(i,j)->makeMove(i,j,x,y)){
+								b->updateMove(i,j,x,y);
+								count =2;
+								return true;
+							}
+						}
+					}
+				}
+				}
+			}
+		}
+	count++;
+	}				
 	return false;
+	}
+	else if(level ==3){
+		return true;
+	}
+	
 }
 
 bool Computer::isCheck(int row, int col){
 	for(int i =0; i<8; i++){
 		for(int j=0; j<8; j++){
-			if(b->theBoard[i][j]->getOwner() == this->playerID()){
-				if(b->theBoard[i][j]->checkMove(i, j, row, col) == true){
+			if(b->getPiece(i,j)->getOwner() == this->playerID()){
+				if(b->getPiece(i,j)->checkMove(i, j, row, col) == true){
 					return true;
 				}
 			}
@@ -239,54 +307,55 @@ bool Computer::isCheckmate(int row, int col){
 	if(isCheck(row, col) == true){
 		int check=0;
 		int availMoves =0;
-		if(b->theBoard[row][col]->checkMove(row, col, row+1, col) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row+1, col) == true){
 			availMoves++;
 			 if(isCheck(row+1, col) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row-1, col) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row-1, col) == true){
 			availMoves++;
 			if(isCheck(row-1, col) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row, col-1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row, col-1) == true){
 			availMoves++;
 			if(isCheck(row, col-1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row, col+1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row, col+1) == true){
 			availMoves++;
 			if(isCheck(row, col+1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row-1, col-1) ==true){
+		if(b->getPiece(i,j)->checkMove(row, col, row-1, col-1) ==true){
 			availMoves++;
 			if(isCheck(row-1, col-1) == true){
 				check++;
 			}		
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row-1, col+1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row-1, col+1) == true){
 			availMoves++;
 			if(isCheck(row-1, col+1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row+1, col-1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row+1, col-1) == true){
 			availMoves++;
 			if(isCheck(row+1, col-1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row+1, col+1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row+1, col+1) == true){
 			availMoves++;
 			if(isCheck(row+1, col+1) == true){
 				check++;
 			}	
 		}
+
 		if(availMoves == check){
 			return true;
 		} else {
--- pawn.cc
+++ pawn.cc
@@ -35,20 +35,24 @@ bool Pawn::checkMove(int row1, int col1, int row2, int col2){
 	//cout <<"attacking a piece" << endl;
 	if(b->getPiece(row2, col2) !=NULL){
 		if((this->getOwner() == 0) && ((row1-row2 == 1) && (abs(col2-col1) == 1))){
+			this->firstmove=false;
 			return true;
 		} else if((this->getOwner() == 1) && ((row2-row1 == 1) && (abs(col2-col1) ==1))){
+			this->firstmove=false;
 			return true;
 		}
 	}
 	//cout << "white moving forward" << endl;
 	if((this->getOwner() ==0) && ((row1-row2) == 1) &&
 		(b->getPiece(row2, col2) == NULL) && (col1==col2)){
+		this->firstmove=false;
 		//cout <<"moving"<<endl;
 		return true;
 	}
 	//cout << "black moving forward" << endl;
 	if((this->getOwner() ==1) && ((row2-row1) == 1) &&
 		(b->getPiece(row2, col2) == NULL) && (col1==col2)){
+		this->firstmove=false;
 		return true;
 	}
 	return false;
