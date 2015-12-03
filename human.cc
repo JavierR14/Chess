346e48d390f757f4158a8f18b7fdb98a43f78557
smcanham <smcanham@student.cs.uwaterloo.ca>
2015-12-03 00:13:49 -0500

    fixed checkmate

--- human.cc
+++ human.cc
@@ -30,9 +30,11 @@ bool Human::movePiece(int row1, int col1, int row2, int col2){
 bool Human::isCheck(int row, int col){
 	for(int i =0; i<8; i++){
 		for(int j=0; j<8; j++){
-			if(b->theBoard[i][j]->getOwner() == this->playerID()){
-				if(b->theBoard[i][j]->checkMove(i, j, row, col) == true){
-					return true;
+			if(b->getPiece(i,j) !=NULL){
+				if(b->getPiece(i,j)->getOwner() == this->playerID()){
+					if(b->theBoard[i][j]->checkMove(i, j, row, col) == true){
+						return true;
+					}
 				}
 			}
 		}
@@ -43,49 +45,49 @@ bool Human::isCheckmate(int row, int col){
 	if(isCheck(row, col) == true){
 		int check=0;
 		int availMoves =0;
-		if(b->theBoard[row][col]->checkMove(row, col, row+1, col) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row+1, col) == true){
 			availMoves++;
 			 if(isCheck(row+1, col) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row-1, col) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row-1, col) == true){
 			availMoves++;
 			if(isCheck(row-1, col) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row, col-1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row, col-1) == true){
 			availMoves++;
 			if(isCheck(row, col-1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row, col+1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row, col+1) == true){
 			availMoves++;
 			if(isCheck(row, col+1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row-1, col-1) ==true){
+		if(b->getPiece(i,j)->checkMove(row, col, row-1, col-1) ==true){
 			availMoves++;
 			if(isCheck(row-1, col-1) == true){
 				check++;
 			}		
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row-1, col+1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row-1, col+1) == true){
 			availMoves++;
 			if(isCheck(row-1, col+1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row+1, col-1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row+1, col-1) == true){
 			availMoves++;
 			if(isCheck(row+1, col-1) == true){
 				check++;
 			}
 		}
-		if(b->theBoard[row][col]->checkMove(row, col, row+1, col+1) == true){
+		if(b->getPiece(i,j)->checkMove(row, col, row+1, col+1) == true){
 			availMoves++;
 			if(isCheck(row+1, col+1) == true){
 				check++;
